d8019cd5a0be6757e33e11d0dac3046e029f118c
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-03-16 00:04:53 +0800

    Added basic function for calculator

---
+++ src/script.js
@@ -0,0 +1,19 @@
+// Get DOM here
+
+
+// Main function and to add later more complicated stuffs
+function add(x, y) {
+    return x + y;    
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
