faf5e36b4e068e7d29c25c3049f44856c921e1e1
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-03-16 23:01:36 +0800

    Added basic functions but there is still so much to do here.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,19 @@
-// Get DOM here
+const outputDisplay = document.querySelector('#currentDisplay');
+const allClear = document.querySelector('#allClear');
+const clear = document.querySelector('#clear');
 
+allClear.addEventListener('click', () => AllClear);
+clear.addEventListener('click', () => Clear);
 
-// Main function and to add later more complicated stuffs
+function AllClear() {
+    console.log("Hello");
+}
+
+function Clear() {
+
+}
+
+// Main functions for basic math
 function add(x, y) {
     return x + y;    
 }
@@ -17,3 +29,21 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+// function operate(operator, x, y) {
+//     x = Number(x)
+//     y = Number(y)
+
+//     switch (operator) {
+//         case '+':
+//             return add(x, y)
+//         case '−':
+//             return substract(x, y)
+//         case '×':
+//             return multiply(x, y)
+//         case '÷':
+//             if (b === 0) return null
+//                 else return divide(a, b)
+//         default:
+//             return null
+// }
