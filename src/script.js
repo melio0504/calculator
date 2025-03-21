9f8e5845a4af749d9d919f525c88bd1ac2f9a998
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-03-21 22:14:43 +0800

    Change the approach in function 'divide', 'operate', and 'roundingNumber'

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-    return roundingNumber(num1 + num2);    
+    return roundingNumber(num1 + num2);
 }
 
 function subtract(num1, num2) {
@@ -11,44 +11,20 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return roundingNumber(num1 / num2);
+    return num2 === 0 ? 'Error' : roundingNumber(num1 / num2);
 }
 
 function operate(operator, num1, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-            break;
-        case '-':
-            return subtract(num1, num2);
-            break;
-        case '*':
-            return multiply(num1, num2);
-            break;
-        case '/':
-            if (num2 === 0) {
-                return 'Error';
-            }
-            return divide(num1, num2);
-            break;
-    }
+    const operations = {
+        '+': add,
+        '-': subtract,
+        '*': multiply,
+        '/': divide,
+    };
+    return operations[operator] ? operations[operator](num1, num2) : 'Error';
 }
 
-// This automatically round the number after doing the main operation.
+// To round a number after operation
 function roundingNumber(num) {
-    const roundLength = 1000000;
-
-    if (!Number.isInteger(num)) {
-        return Math.round((num + Number.EPSILON) * roundLength) / roundLength;
-    }
-
-    return num;
+    return Number.isInteger(num) ? num : parseFloat(num.toFixed(6));
 }
-
-let firstNum = 0;
-let secondNum = 0;
-let outputDisplay = '';
-
-const display = document.querySelector('#currentDisplay')
-
-// Find a way to create an EventListener that when the user clicked a specific button it will append in the outputDisplay
